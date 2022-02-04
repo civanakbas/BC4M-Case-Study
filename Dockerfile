72086b50482622f7753db39e0260286aad05fab0
civanakbas <civan.akbas@hotmail.com>
2022-02-04 23:54:11 +0300

    deploy heroku try#2

--- Dockerfile
+++ Dockerfile
@@ -8,6 +8,4 @@ RUN pip3 install -r requirements.txt
 
 COPY . .
 
-EXPOSE 8080
-
 CMD [ "python3", "./src/app.py"]
--- src/app.py
+++ src/app.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from routes import configure_routes
+import os
 
 app = Flask(__name__)
 
 configure_routes(app)
 
 if __name__ == "__main__":
-    app.run(debug=True, port=8080, host="0.0.0.0")
+    port = os.environ.get("PORT", 5000)
+    app.run(debug=True, port=port, host="0.0.0.0")
